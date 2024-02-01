1246b47b35e50f42e6a58eb9ed12df32157877fb
Katsumi Kato <katsumi.kato@litalico.co.jp>
2024-02-02 00:01:49 +0900

    :white_check_mark: Testing of Validation Rule Generation Trait

---
+++ tests/FullAccessWrapper.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use ErrorException;
+use ReflectionClass;
+use ReflectionException;
+
+/**
+ * Class for testing private, protected methods and properties
+ */
+class FullAccessWrapper
+{
+    /** @var ReflectionClass reflection */
+    private ReflectionClass $reflection;
+
+    /**
+     * @param object $targetInstance
+     */
+    public function __construct(private readonly object $targetInstance)
+    {
+        $this->reflection = new ReflectionClass($targetInstance);
+    }
+
+    /**
+     * @param string $methodName
+     * @param array<int,mixed> $args
+     * @return mixed
+     * @throws ReflectionException
+     */
+    public function __call(string $methodName, array $args)
+    {
+        $method = $this->reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($this->targetInstance, $args);
+    }
+
+    /**
+     * @param string $name
+     * @return mixed|void
+     * @throws ReflectionException
+     * @throws ErrorException
+     */
+    public function __get(string $name)
+    {
+        if ($this->reflection->hasProperty($name)) {
+            $property = $this->reflection->getProperty($name);
+            $property->setAccessible(true);
+
+            return $property->getValue($this->targetInstance);
+        }
+
+        throw new ErrorException("ErrorException : Undefined property: $name");
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     * @throws ErrorException
+     * @throws ReflectionException
+     */
+    public function __set(string $name, mixed $value): void
+    {
+        if ($this->reflection->hasProperty($name)) {
+            $property = $this->reflection->getProperty($name);
+
+            $property->setValue($this->targetInstance, $value);
+        } else {
+            throw new ErrorException("ErrorException : Undefined property: $name");
+        }
+    }
+
+    /**
+     * Get reflection source instance
+     * @return Object
+     */
+    public function getInstance(): Object
+    {
+        return $this->targetInstance;
+    }
+}
