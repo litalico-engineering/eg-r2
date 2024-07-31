3d754c433342b6be8ede1e5b1c89715fdbf3f79c
k2tzumi <katzumi+github@gmail.com>
2024-07-31 13:00:00 +0900

    :sparkles: Mapping requests to nested types

--- src/Http/Requests/FormRequestPropertyHandlerTrait.php
+++ src/Http/Requests/FormRequestPropertyHandlerTrait.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Litalico\EgR2\Http\Requests;
 
+use Illuminate\Foundation\Http\FormRequest;
+use InvalidArgumentException;
 use ReflectionClass;
 use ReflectionNamedType;
 use ReflectionProperty;
+use ReflectionType;
+use ReflectionException;
 
 /**
  * If the OpenApi attribute is embedded in the form request class along  with the Property addition,
@@ -20,6 +24,7 @@ trait FormRequestPropertyHandlerTrait
      * Handle a passed validation attempt.
      * @see https://readouble.com/laravel/10.x/en/validation.html#preparing-input-for-validation
      * @return void
+     * @throws ReflectionException
      */
     protected function passedValidation(): void
     {
@@ -33,24 +38,70 @@ trait FormRequestPropertyHandlerTrait
             }
 
             $value = request($property->getName());
+            $type = $property->getType();
             if ($value === null) {
-                $type = $property->getType();
-                if ($type instanceof ReflectionNamedType) {
-                    if ($type->allowsNull()) {
-                        // TODO: If an item specified as nullable or default in the OpenAPI specification is not entered, set the default value to Property.
-                        $value = null;
-                    } else {
-                        $value = match ($type->getName()) {
-                            'array' => [],
-                            'int' => 0,
-                            default => ''
-                        };
-                    }
-                } else {
-                    $value = '';
+                $value = $this->initialValue($type);
+            } else {
+                if (!$type->isBuiltin()) {
+                    $value = $this->initializationFormRequest($type->getName(), $value);
                 }
             }
             $property->setValue($this, $value);
         }
+    }
+
+    /**
+     * @param ReflectionType|null $type
+     * @return mixed|null
+     * @throws ReflectionException
+     */
+    private function initialValue(?ReflectionType $type)
+    {
+        if ($type instanceof ReflectionNamedType) {
+            if ($type->allowsNull()) {
+                return null;
+            } else {
+                if ($type->isBuiltin()) {
+                    return match ($type->getName()) {
+                        'array' => [],
+                        'int' => 0,
+                        default => ''
+                    };
+                } else {
+                    return $this->initializationFormRequest($type->getName());
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param class-string<FormRequest> $class
+     * @param array $requestValues
+     * @return FormRequest
+     * @throws ReflectionException
+     */
+    private function initializationFormRequest(string $class, array $requestValues = []): FormRequest
+    {
+        $instance = new $class();
+
+        if (($instance instanceof FormRequest) === false) {
+            throw new InvalidArgumentException('The class must be an instance of FormRequest');
+        }
+
+        $reflection = new ReflectionClass($class);
+        $properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+
+        foreach ($properties as $property) {
+            $type = $property->getType();
+            if (isset($requestValues[$property->getName()])) {
+                $property->setValue($instance, $requestValues[$property->getName()]);
+            } else {
+                $property->setValue($instance, $this->initialValue($type));
+            }
+        }
+
+        return $instance;
     }
 }
--- tests/Unit/FormRequestPropertyHandlerTraitTest.php
+++ tests/Unit/FormRequestPropertyHandlerTraitTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Litalico\EgR2\Http\Requests\FormRequestPropertyHandlerTrait;
+use Mockery;
 use OpenApi\Attributes\Property;
 use Tests\FullAccessWrapper;
 use Tests\TestCase;
@@ -89,4 +90,168 @@ class FormRequestPropertyHandlerTraitTest extends TestCase
         self::assertNull($instance->nullable);
         self::assertEmpty($instance->unknownType);
     }
+
+
+    /**
+     * @test
+     * @covers ::passedValidation
+     */
+    public function testGetPropertiesOfNestedObject(): void
+    {
+        setup:
+        $requestMock = Mockery::mock('Illuminate\Http\Request');
+        $requestMock->shouldReceive('setUserResolver')->andReturn("dummy");
+        $requestMock->shouldReceive('all')->andReturn(['id' => 1, 'name' => 'bob']);
+        $this->app->instance('request', $requestMock);
+
+        $class = new class extends FormRequest {
+            use FormRequestPropertyHandlerTrait;
+
+            #[Property(
+                property: "id",
+                title: "id",
+                description: "id",
+                type: "integer",
+                format: "int",
+                minimum: 1
+            )]
+            public int $id;
+
+            #[Property(
+                property: "name",
+                title: "name",
+                description: "name",
+                type: "string"
+            )]
+            public string $name;
+        };
+        /** @var $instance FormRequestPropertyHandlerTrait */
+        $instance = new FullAccessWrapper($class);
+
+        when:
+        $instance->passedValidation();
+
+        then:
+        self::assertNotNull($instance);
+        self::assertEquals($instance->id, 1);
+        self::assertEquals($instance->name, 'bob');
+    }
+
+    /**
+     * @test
+     * @covers ::passedValidation
+     */
+    public function testGetNestedObjectPropertiesEvenIfValueIsNull(): void
+    {
+        setup:
+        $class = new class extends FormRequest {
+            use FormRequestPropertyHandlerTrait;
+
+            #[Property(
+                property: "id",
+                title: "id",
+                description: "id",
+                type: "integer",
+                format: "int",
+                minimum: 1
+            )]
+            public int $id;
+
+            #[
+                Property(
+                    property: 'nested',
+                    ref: '#/components/schemas/NestedObject',
+                ),
+            ]
+            public NestedObject $nested;
+        };
+        /** @var $instance FormRequestPropertyHandlerTrait */
+        $instance = new FullAccessWrapper($class);
+
+        when:
+        $instance->passedValidation();
+
+        then:
+        self::assertNotNull($instance);
+        self::assertEquals($instance->id, 0);
+        self::assertNotNull($instance->nested);
+        self::assertInstanceOf(NestedObject::class, $instance->nested);
+        self::assertEquals($instance->nested->id, 0);
+        self::assertEmpty($instance->nested->name);
+    }
+
+    /**
+     * @test
+     * @covers ::passedValidation
+     * @throws \ReflectionException
+     */
+    public function testNestedObject(): void
+    {
+        setup:
+        $requestMock = Mockery::mock('Illuminate\Http\Request');
+        $requestMock->shouldReceive('setUserResolver')->andReturn("dummy");
+        $requestMock->shouldReceive('all')->andReturn(['id' => 1, 'nested' => ['id' => 2, 'name' => 'bob']]);
+        $this->app->instance('request', $requestMock);
+
+        $class = new class extends FormRequest {
+            use FormRequestPropertyHandlerTrait;
+
+            #[Property(
+                property: "id",
+                title: "id",
+                description: "id",
+                type: "integer",
+                format: "int",
+                minimum: 1
+            )]
+            public int $id;
+
+            #[
+                Property(
+                    property: 'nested',
+                    ref: '#/components/schemas/NestedObject',
+                ),
+            ]
+            public NestedObject $nested;
+        };
+        /** @var $instance FormRequestPropertyHandlerTrait */
+        $instance = new FullAccessWrapper($class);
+
+        when:
+        $instance->passedValidation();
+
+        then:
+        self::assertNotNull($instance);
+        self::assertEquals($instance->id, 1);
+        self::assertNotNull($instance->nested);
+        self::assertInstanceOf(NestedObject::class, $instance->nested);
+        self::assertEquals($instance->nested->id, 2);
+        self::assertEquals($instance->nested->name, 'bob');
+    }
+}
+
+#[\OpenApi\Attributes\Schema(
+    schema: 'NestedObject',
+    required: ['id']
+)]
+class NestedObject extends FormRequest
+{
+    #[Property(
+        property: "id",
+        title: "id",
+        description: "id",
+        type: "integer",
+        format: "int",
+        minimum: 1
+    )]
+    public int $id;
+
+    #[Property(
+        property: "name",
+        title: "name",
+        description: "name",
+        type: "string",
+        format: "string"
+    )]
+    public string $name;
 }
